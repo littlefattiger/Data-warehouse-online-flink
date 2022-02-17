756fb3b73a54ff6fad6ed5fe202de26038673eca
lilltfattiger <6698381@qq.com>
2022-02-16 20:09:15 -0700

    update file until p50

--- gmall-realtime/src/main/java/com/atguigu/app/ods/FlinkCDC.java
+++ gmall-realtime/src/main/java/com/atguigu/app/ods/FlinkCDC.java
@@ -5,28 +5,21 @@ import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
 import com.atguigu.app.function.CustomerDeserialization;
 import com.atguigu.utils.MyKafkaUtil;
-import org.apache.flink.runtime.state.filesystem.FsStateBackend;
-import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.kafka.common.protocol.types.Field;
 
 public class FlinkCDC {
-
     public static void main(String[] args) throws Exception {
-
-        //1.获取执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        //1.1 设置CK&状态后端
-        //env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/gmall-flink-210325/ck"));
-        //env.enableCheckpointing(5000L);
-        //env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
-        //env.getCheckpointConfig().setCheckpointTimeout(10000L);
-        //env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
-        //env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000);
-
-        //env.setRestartStrategy(RestartStrategies.fixedDelayRestart());
+//        env.setStateBackend(new FsStateBackend("hdfs://hadoop102:8020/gmall-flink-210325/ck"));
+//        env.enableCheckpointing(5000L);
+//        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setCheckpointTimeout(10000L);
+//        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+//        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000);
 
         //2.通过FlinkCDC构建SourceFunction并读取数据
         DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
@@ -35,18 +28,19 @@ public class FlinkCDC {
                 .username("root")
                 .password("111111")
                 .databaseList("gmall-210325-flink")
+//                .tableList("gmall-210325-flink.base_trademark")   //如果不添加该参数,则消费指定数据库中所有表的数据.如果指定,指定方式为db.table
                 .deserializer(new CustomerDeserialization())
                 .startupOptions(StartupOptions.latest())
                 .build();
+        String sinkTopic = "ods_base_db";
         DataStreamSource<String> streamSource = env.addSource(sourceFunction);
 
-        //3.打印数据并将数据写入Kafka
-        streamSource.print();
-        String sinkTopic = "ods_base_db";
+
+//        streamSource.print();
         streamSource.addSink(MyKafkaUtil.getKafkaProducer(sinkTopic));
 
         //4.启动任务
         env.execute("FlinkCDC");
-    }
 
+    }
 }
